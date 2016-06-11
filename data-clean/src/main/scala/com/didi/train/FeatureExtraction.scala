e745a6c575632b171779b77a3f325197a8abd1c9
fcbai <piaobomengxiang@163.com>
2016-06-11 23:59:31 +0800

    [#2] --fcbai add feature extraction method

--- data-clean/src/main/scala/com/didi/train/FeatureExtraction.scala
+++ data-clean/src/main/scala/com/didi/train/FeatureExtraction.scala
@@ -1,6 +1,12 @@
 package com.didi.train
 
+import com.didi.models.{OrderFeatureTable, Table}
+
 
 object FeatureExtraction {
-
+  def extract(table: Table): OrderFeatureTable = {
+    table match {
+      case _ => OrderFeatureTable()
+    }
+  }
 }
