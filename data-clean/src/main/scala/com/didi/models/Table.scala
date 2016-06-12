677b39bf1f15777170cd313d716a889191d3a40c
fcbai <piaobomengxiang@163.com>
2016-06-12 09:19:53 +0800

    [N/A] --fcbai add full order table struct

--- data-clean/src/main/scala/com/didi/models/Table.scala
+++ data-clean/src/main/scala/com/didi/models/Table.scala
@@ -15,6 +15,22 @@ case class Traffic(districtHash: String, tjLevel: String, tjTime: String) extend
 
 case class Weather(time: String, weather: Int, temperature: Double, pm2p5: Double) extends Table
 
-final case class FullOrder() extends Table
+final case class FullOrder(
+                           orderId: String,
+                           driverId: String,
+                           passengerId: String,
+                           startRegion: String,
+                           startRegionId: String,
+                           startRegionTjLevel: String,
+                           destRegion: String,
+                           destRegionId: String,
+                           destRegionTjLevel: String,
+                           price: Double,
+                           weather: Int,
+                           temperature: Double,
+                           pm2p5: Double,
+                           poiClass: String,
+                           time: String
+                          ) extends Table
 
 final case class OrderFeatureTable() extends FeatureTable
