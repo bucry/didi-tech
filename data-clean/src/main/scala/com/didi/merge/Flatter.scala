0844d996a2290d524c778e44b8856bb9c3edc62d
fcbai <piaobomengxiang@163.com>
2016-06-12 21:00:50 +0800

    [#1] --fcbai add join type

--- data-clean/src/main/scala/com/didi/merge/Flatter.scala
+++ data-clean/src/main/scala/com/didi/merge/Flatter.scala
@@ -1,8 +1,33 @@
 package com.didi.merge
 
-/**
-  * Created by twcn on 6/9/16.
-  */
-class Flatter {
+import org.apache.spark.sql.DataFrame
+
+object Flatter {
+
+  abstract class Join(column: String)
+
+  final case class Left(column: String) extends Join(column)
+  final case class Right(column: String) extends Join(column)
+  final case class Inner(column: String) extends Join(column)
+  final case class Out(column: String) extends Join(column)
+
+
+  def join(letDataFrame: DataFrame, rightDataFrame: DataFrame, where: Join): DataFrame = {
+    where match {
+      case Left(column) => joinLeft(letDataFrame, rightDataFrame, column)
+      case Right(column) => joinRight(letDataFrame, rightDataFrame, column)
+      case _ => letDataFrame
+    }
+
+
+  }
+
+  private def joinLeft(letDataFrame: DataFrame, rightDataFrame: DataFrame, column: String): DataFrame = {
+    letDataFrame.join(rightDataFrame, letDataFrame(column) === rightDataFrame(column), "left")
+  }
+
+  private def joinRight(letDataFrame: DataFrame, rightDataFrame: DataFrame, column: String): DataFrame = {
+    letDataFrame.join(rightDataFrame, letDataFrame(column) === rightDataFrame(column), "right")
+  }
 
 }
