6c7fa260839392908a0a3029fc1de5d01737ea1c
fcbai <piaobomengxiang@163.com>
2016-06-12 21:01:21 +0800

    [#2] --fcbai update TSV file path to v2

--- data-clean/src/main/scala/com/didi/config/TsvFilePath.scala
+++ data-clean/src/main/scala/com/didi/config/TsvFilePath.scala
@@ -6,9 +6,9 @@ package com.didi.config
 
 //TODO you should modify the variable to using your file path
 object TsvFilePath {
-  val orderFilePath = "/Users/twcn/Downloads/season_1/training_data/order_data/order_data_*"
-  val trafficFilePath = "/Users/twcn/Downloads/season_1/training_data/traffic_data/traffic_data_*"
-  val clusterFilePath = "/Users/twcn/Downloads/season_1/training_data/cluster_map/cluster_map"
-  val poiFilePath = "/Users/twcn/Downloads/season_1/training_data/poi_data/poi_data"
-  val weatherFilePath = "/Users/twcn/Downloads/season_1/training_data/weather_data/weather_data_*"
+  val orderFilePath = "/Users/twcn/Downloads/season_2/training_data/order_data/order_data_*"
+  val trafficFilePath = "/Users/twcn/Downloads/season_2/training_data/traffic_data/traffic_data_*"
+  val clusterFilePath = "/Users/twcn/Downloads/season_2/training_data/cluster_map/cluster_map"
+  val poiFilePath = "/Users/twcn/Downloads/season_2/training_data/poi_data/poi_data"
+  val weatherFilePath = "/Users/twcn/Downloads/season_2/training_data/weather_data/weather_data_*"
 }
