214255181189b6bdfa4f01f4cb393fbf3530567b
fcbai <piaobomengxiang@163.com>
2016-06-16 23:44:12 +0800

    [N/A] --fcbai ad deeplearning demo

---
+++ data-clean/src/main/scala/com/didi/spike/MllibSpike.scala
@@ -0,0 +1,8 @@
+package com.didi.spike
+
+/**
+  * Created by twcn on 6/16/16.
+  */
+class MllibSpike {
+
+}
---
+++ data-clean/src/main/scala/com/didi/train/Wrapper.scala
@@ -0,0 +1,13 @@
+package com.didi.train
+
+/**
+  * Created by twcn on 6/16/16.
+  */
+object Wrapper {
+
+  final val EnumMetadataName: String = "isEnum"
+  final val LabelMetadataName: String = "isLable"
+  final val EnumColumnTail: String = "_Enum"
+  final val VectorColumnTail: String = "_Vector"
+
+}
